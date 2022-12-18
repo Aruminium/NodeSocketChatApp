ea72b6079bad1251e4532809332c1539fb4304e8
Aruminium <b2201680@photon.chitose.ac.jp>
2022-12-18 11:30:22 +0900

    不要なnpmライブラリのアンインストール(#1)

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-app.get("/", (req, res) => {
+app.get("/", (req: any, res: any) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
-  socket.on("chat message", (msg) => {
+io.on("connection", (socket: any) => {
+  socket.on("chat message", (msg: any) => {
     io.emit("chat message", msg);
   });
 });
